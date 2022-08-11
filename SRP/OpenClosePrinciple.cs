7f8b193e4558db37a5e40b69f6bbd019cfdb4d10
Oluwaseyi0910 <110403016+Oluwaseyi0910@users.noreply.github.com>
2022-08-11 05:34:04 +0100

    Add project files.

---
+++ SRP/OpenClosePrinciple.cs
@@ -0,0 +1,73 @@
+﻿using SRP;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCP
+{
+    public class ReportGeneration
+    {
+        /// <summary>
+        /// Report type
+        /// </summary>
+        public string ReportType { get; set; }
+
+        /// <summary>
+        /// Method to generate report
+        /// </summary>
+        /// <param name="em"></param>
+        public void GenerateReport(Employee em)
+        {
+            try
+            {
+                // to create an exception
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+            if (ReportType == "CRS")
+            {
+                // Report generation with employee data in Crystal Report.
+            }
+            if (ReportType == "PDF")
+            {
+                // Report generation with employee data in PDF.
+            }
+        }
+    }
+    public class IReportGeneration
+    {
+        /// <summary>
+        /// Method to generate report
+        /// </summary>
+        /// <param name="em"></param>
+        public virtual void GenerateReport(Employee em)
+        {
+            // From base
+        }
+    }
+    /// <summary>
+    /// Class to generate Crystal report
+    /// </summary>
+    public class CrystalReportGeneraion : IReportGeneration
+    {
+        public override void GenerateReport(Employee em)
+        {
+            // Generate crystal report.
+        }
+    }
+    /// <summary>
+    /// Class to generate PDF report
+    /// </summary>
+    public class PDFReportGeneraion : IReportGeneration
+    {
+        public override void GenerateReport(Employee em)
+        {
+            // Generate PDF report.
+        }
+    }
+}
---
+++ SRP/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    public class Employee
+    {                       
+        public int Employee_Id { get; set; }
+        public string Employee_Name { get; set; }
+
+        /// <summary>
+        /// This method used to insert into employee table
+        /// </summary>
+        /// <param name="em">Employee object</param>
+        /// <returns>Successfully inserted or not</returns>
+        public bool InsertIntoEmployeeTable(Employee em)
+        {
+            try
+            {
+                // to create an exception
+            }
+            catch (Exception e) 
+            {
+
+                throw;
+            }
+            // Insert into employee table.
+            return true;
+        }
+        /// <summary>
+        /// Method to generate report
+        /// </summary>
+        /// <param name="em"></param>
+        public void GenerateReport(Employee em)
+        {
+            // Report generation with employee data using crystal report.
+        }
+    }
+    public class ReportGeneration
+    {
+        /// <summary>
+        /// Method to generate report
+        /// </summary>
+        /// <param name="em"></param>
+        public void GenerateReport(Employee em)
+        {
+            // Report reneration with employee data.
+        }
+    }
+}
+
